06b541464877ad2e4921b43570b2b5f9d1e4d3a1
Gift Richard <giftrichard2000@gmail.com>
2025-02-16 16:22:06 +0100

    ss

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { HashRouter as Router, Routes, Route } from 'react-router-dom';
+import {  Routes, Route } from 'react-router-dom';
 import Home from "./pages/Home/Home";
 import './App.css';
 import { useEffect } from "react";
